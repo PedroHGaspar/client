698c09ad3f6e1a98f1a4724dc955c256d5f6502d
PedroHGaspar <ordep12@hotmail.com.br>
2023-05-25 13:19:03 -0300

    asdd

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = React.useState(null);
+  const [email, setEmail] = React.useState(null);
+  const [cpf, setCpf] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.message);
+        setEmail(data.email);
+        setCpf(data.cpf);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{!data ? "Loading..." : data}</p>
+        <p>Email: {!email ? "Loading..." : email}</p>
+        <p>CPF: {!cpf ? "Loading..." : cpf}</p>
       </header>
     </div>
   );
